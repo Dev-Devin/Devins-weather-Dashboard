b4074a8bd5766c7a614e900981e2d5979a2c851a
Devin Polichetti <devinpolichetti@devins-mbp.lan>
2022-11-06 17:40:03 -0500

    adding JS, trying to get search bar to work with API

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,10 @@ let weather = {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
-        " & units=metric&appid=" +
+        "&units=metric&appid=" +
         this.apiKey
     )
-      .then((Response) => Response.json())
+      .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
   displayWeather: function (data) {
@@ -15,9 +15,23 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity);
+    console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".city").innerText = "weather in " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+    weather.search();
   },
 };
+
+document.querySelectorAll(".search-bar").addEventListener("click", function () {
+  weather.search();
+});
