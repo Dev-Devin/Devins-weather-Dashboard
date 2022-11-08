7ae4aca9ee2bbc0bef7cd305fc404afe4c71b061
Devin Polichetti <devinpolichetti@devins-mbp.lan>
2022-11-08 08:16:07 -0500

    readme, index, html

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,18 @@
 let weather = {
   apiKey: "3687cc076ea1bdcd40a2b67ec12296e3",
   fetchWeather: function (city) {
+    console.log("city= ", city);
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
-        "&units=metric&appid=" +
+        "&units=imperial&appid=" +
         this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
+      .then((data) => {
+        console.log("data= ", data);
+        this.displayWeather(data);
+      });
   },
   displayWeather: function (data) {
     const { name } = data;
@@ -20,18 +24,18 @@ let weather = {
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + "@2x.png";
     document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".temp").innerText = temp + "°F";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
-    weather.search();
+    // weather.search();
   },
 };
 
-document.querySelectorAll(".search-bar").addEventListener("click", function () {
+document.querySelector("#search-btn").addEventListener("click", function () {
+  console.log("click call back");
   weather.search();
 });
